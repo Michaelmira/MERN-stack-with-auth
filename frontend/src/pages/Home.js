2634eb7768691d2d7c04ab73746c4868e6e20e31
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-06-04 04:14:42 +0000

    Link so Authorized users only can see workouts and details

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,16 +1,23 @@
 import { useEffect } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // componets
 import WorkoutDetails from '../componets/WorkoutDetails'
 import WorkoutForm from '../componets/WorkoutForm'
 
+
 const Home = () => {
     const {workouts, dispatch} = useWorkoutsContext()
+    const {user} = useAuthContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch("/api/workouts/")
+            const response = await fetch("/api/workouts/", {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
 
             if (response.ok) {
@@ -18,8 +25,11 @@ const Home = () => {
             }
         }
 
-        fetchWorkouts()
-    }, [dispatch])
+        if (user) {
+            fetchWorkouts()
+        }
+    }, [dispatch, user])
+
     return (
         <div className="home">
             <div className='workouts'>
